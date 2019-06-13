065702ba4de60a66b1ab25d90f7ecfa112ab4c57
ggaly <1689392164@qq.com>
2019-06-13 14:40:31 +0800

    redlock

---
+++ springcloud-redlock/src/main/java/com/gly/springcloudredlock/SpringcloudRedlockApplication.java
@@ -0,0 +1,13 @@
+package com.gly.springcloudredlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudRedlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudRedlockApplication.class, args);
+    }
+
+}
---
+++ springcloud-redlock/src/test/java/com/gly/springcloudredlock/SpringcloudRedlockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gly.springcloudredlock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudRedlockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
